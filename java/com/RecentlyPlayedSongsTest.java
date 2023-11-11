991236fd257aeccbac99ba9597b98f8bef174b5b
Arun G <69797268+arun-qa@users.noreply.github.com>
2023-11-11 12:51:20 +0530

    Add files via upload

--- java/com/RecentlyPlayedSongsTest.java
+++ java/com/RecentlyPlayedSongsTest.java
@@ -21,19 +21,19 @@ public class RecentlyPlayedSongsTest {
 	@Test
 	public void test2()
 	{
-		RecentlyPlayedSongs songs=new RecentlyPlayedSongs(3);
+		RecentlyPlayedSongs songs=new RecentlyPlayedSongs(4);
         songs.playSong("user1","S1");
         songs.playSong("user1","S2");
         songs.playSong("user1","S3");
         songs.playSong("user1","S4");
         songs.playSong("user1","S3");
+        songs.playSong("user2","S1");
+        songs.playSong("user2","S2");
         songs.playSong("user2","S3");
         songs.playSong("user2","S4");
         songs.playSong("user2","S5");
-        songs.playSong("user2","S3");
-        songs.playSong("user2","S6");
-        Assert.assertEquals(Arrays.asList("S2","S4","S3"),songs.getRecentlyPlayedSongs("user1"));
-        Assert.assertEquals(Arrays.asList("S5","S3","S6"),songs.getRecentlyPlayedSongs("user2"));
+        Assert.assertEquals(Arrays.asList("S1","S2","S4","S3"),songs.getRecentlyPlayedSongs("user1"));
+        Assert.assertEquals(Arrays.asList("S2","S3","S4","S5"),songs.getRecentlyPlayedSongs("user2"));
 	}
 
 }
